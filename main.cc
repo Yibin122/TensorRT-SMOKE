fbb7c0eebe11f30a883fc584a2b0d5130c10d397
Yibin122 <56641930+Yibin122@users.noreply.github.com>
2022-05-01 16:33:16 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,12 +1,11 @@
+#include "smoke.hh"
 #include <opencv2/imgcodecs.hpp>
 
-#include "smoke.hh"
-
 int main(int argc, char** argv) {
-    cv::Mat kitti_img = cv::imread("../kitti_000008.png");
+    cv::Mat kitti_img = cv::imread("../samples/kitti_000008.png");
     cv::Mat intrinsic = (cv::Mat_<float>(3, 3) << 
         721.5377, 0.0, 609.5593, 0.0, 721.5377, 172.854, 0.0, 0.0, 1.0);
-    SMOKE smoke("../smoke.trt8", intrinsic);
+    SMOKE smoke("../smoke_dla34.trt8", intrinsic);
     smoke.Detect(kitti_img);
     return 0;
 }
